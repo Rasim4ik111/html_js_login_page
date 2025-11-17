daffaa5b6dd8c55236ce6ff78613d6a6a6dda659
Rasim <rasim95suleymanli@gmail.com>
2025-11-17 09:24:15 +0400

    add some style

--- main.js
+++ main.js
@@ -6,11 +6,16 @@ btn.addEventListener('click', function () {
     const admin = 'admin';
     const password_admin = 'admin';
 
+    if (login.length === 0 || password.length === 0) {
+        alert("Melumatlarinizi daxil edin");
+        return;
+    }
+
 
     if (login == admin && password == password_admin) {
-        console.log("Siz daxil olduz")
+        alert("Siz daxil olduz")
     } else {
-        console.log("Qeydiyyat edin")
+        alert("Qeydiyyat edin")
     }
 
 })
